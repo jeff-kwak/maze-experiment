15926a7b51a4ccce0b1e93820bfcc482e2109452
jeffk <jeff.kwak@gmail.com>
2023-04-20 21:49:24 -0400

    chore: improved maze canvas

--- Util/maze_canvas.gd
+++ Util/maze_canvas.gd
@@ -2,18 +2,7 @@ extends Node2D
 
 
 var _maze: Maze
-var _width: int = 0
-var _height: int = 0
-var _cell_size: int = 16
-var _wall_thickness: int = 5
-var _even_cell_color: Color = Color.WHITE
-var _odd_cell_color: Color = Color.WHITE
-var _wall_color: Color = Color.BLACK
-var _path_color: Color = Color.BLACK
-var _draw_gradient: bool = false
-var _draw_entrance_exit: bool = false
-var _draw_solution: bool = false
-
+var _settings: MazeSettings
 
 func _ready():
     get_viewport().size_changed.connect(_on_resized)
@@ -24,47 +13,33 @@ func _on_resized():
 
 
 func _draw():
-    for x in _width:
-        for y in _height:
+    for x in _settings.width:
+        for y in _settings.height:
             _draw_cell(Vector2i(x, y))
 
-    for x in _width:
-        for y in _height:
+    for x in _settings.width:
+        for y in _settings.height:
             _draw_walls(Vector2i(x, y))
 
-    print("draw entrance exit: %s" % _draw_entrance_exit)
-    print("draw solution: %s" % _draw_solution)
-
-    if _draw_entrance_exit:
+    if _settings.draw_entrance_exit:
         _draw_the_entrance_and_exit()
 
-    if _draw_solution:
+    if _settings.draw_solution:
         _draw_the_solution()
 
 
 func draw_maze(maze: Maze, maze_settings: MazeSettings) -> void:
     _maze = maze
-    _width = maze.grid_width
-    _height = maze.grid_height
-    _cell_size = maze_settings.cell_size
-    _wall_thickness = maze_settings.wall_width
-    _even_cell_color = maze_settings.even_cell_color
-    _odd_cell_color = maze_settings.odd_cell_color
-    _wall_color = maze_settings.wall_color
-    _path_color = maze_settings.path_color
-    _draw_gradient = maze_settings.draw_gradient
-    _draw_entrance_exit = maze_settings.draw_entrance_exit
-    _draw_solution = maze_settings.draw_solution
+    _settings = maze_settings
     queue_redraw()
 
 
 func _rect(cell: Vector2i) -> Rect2:
-    var p1 = Vector2(cell.x * _cell_size, cell.y * _cell_size)
-    var p2 = Vector2(_cell_size, _cell_size)
+    var p1 = Vector2(cell.x * _settings.cell_size, cell.y * _settings.cell_size)
+    var p2 = Vector2(_settings.cell_size, _settings.cell_size)
     return Rect2(p1, p2)
 
 func _draw_cell(cell: Vector2i) -> void:
-    # draw the floor
     var rect = _rect(cell)
     var color = _calculate_cell_color(cell)
     draw_rect(rect, color, true)
@@ -75,44 +50,44 @@ func _draw_walls(cell: Vector2i) -> void:
     var p1 = rect.position
     var walls = _maze.grid_at(cell)
     var top_left: Vector2 = p1
-    var top_right: Vector2 = top_left + Vector2(_cell_size, 0)
-    var bottom_left: Vector2 = top_left + Vector2(0, _cell_size)
-    var bottom_right: Vector2 = bottom_left + Vector2(_cell_size, 0)
+    var top_right: Vector2 = top_left + Vector2(_settings.cell_size, 0)
+    var bottom_left: Vector2 = top_left + Vector2(0, _settings.cell_size)
+    var bottom_right: Vector2 = bottom_left + Vector2(_settings.cell_size, 0)
 
-    var wt: float = _wall_thickness * 0.5
+    var wt: float = _settings.wall_width * 0.5
     var x_nudge = Vector2(wt, 0)
     var y_nudge = Vector2(0, wt)
 
     if walls & Maze.Wall.North:
-        draw_line(top_left - x_nudge, top_right + x_nudge, _wall_color, _wall_thickness)
+        draw_line(top_left - x_nudge, top_right + x_nudge, _settings.wall_color, _settings.wall_width)
 
     if walls & Maze.Wall.South:
-        draw_line(bottom_left - x_nudge, bottom_right + x_nudge, _wall_color, _wall_thickness)
+        draw_line(bottom_left - x_nudge, bottom_right + x_nudge, _settings.wall_color, _settings.wall_width)
 
     if walls & Maze.Wall.West:
-        draw_line(top_left - y_nudge, bottom_left + y_nudge, _wall_color, _wall_thickness)
+        draw_line(top_left - y_nudge, bottom_left + y_nudge, _settings.wall_color, _settings.wall_width)
 
     if walls & Maze.Wall.East:
-        draw_line(top_right - y_nudge, bottom_right + y_nudge, _wall_color, _wall_thickness)
+        draw_line(top_right - y_nudge, bottom_right + y_nudge, _settings.wall_color, _settings.wall_width)
 
 func _calculate_cell_color(cell: Vector2i) -> Color:
-     if _draw_gradient:
+     if _settings.draw_gradient:
         var max_dist = _maze.get_max_distance()
         var cell_dist = _maze.dist_at(cell)
         var weight = cell_dist/float(max_dist)
-        var result = lerp(_odd_cell_color, _even_cell_color, weight)
+        var result = lerp(_settings.odd_cell_color, _settings.even_cell_color, weight)
         return result
 
      else:
-        return _even_cell_color if (cell.x + cell.y) % 2 == 0 else _odd_cell_color
+        return _settings.even_cell_color if (cell.x + cell.y) % 2 == 0 else _settings.odd_cell_color
 
 
 func _draw_the_entrance_and_exit() -> void:
     var entrance: Rect2 = _rect(_maze.entrance)
-    draw_circle(entrance.position + 0.5 * entrance.size, _wall_thickness, _path_color)
+    draw_circle(entrance.position + 0.5 * entrance.size, _settings.wall_width, _settings.path_color)
     var exit: Rect2 = _rect(_maze.exit)
-    var exit_pos = exit.position + Vector2(_cell_size * 0.5 - _wall_thickness, _cell_size * 0.5 - _wall_thickness)
-    draw_rect(Rect2(exit_pos, Vector2(_wall_thickness * 2, _wall_thickness * 2)), _path_color)
+    var exit_pos = exit.position + Vector2(_settings.cell_size * 0.5 - _settings.wall_width, _settings.cell_size * 0.5 - _settings.wall_width)
+    draw_rect(Rect2(exit_pos, Vector2(_settings.wall_width * 2, _settings.wall_width * 2)), _settings.path_color)
 
 
 func _find_next_in_path(from: Vector2i, to_cells: Array) -> Vector2i:
@@ -142,5 +117,5 @@ func _draw_the_solution():
         .map(func(p): return _rect(p)) \
         .map(func(r): return r.position + 0.5 * r.size)
 
-    draw_polyline(points, _path_color, _wall_thickness * 0.62)
+    draw_polyline(points, _settings.path_color, _settings.wall_width * 0.62)
 
